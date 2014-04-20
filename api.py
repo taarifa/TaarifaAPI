49c15bc44eb68dd5ae045487397a3361db532f33
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-20 18:21:46 +0100

    Hello world from Eve

---
+++ api.py
@@ -0,0 +1,17 @@
+from os import environ
+
+from eve import Eve
+
+from settings import API_NAME
+
+api = Eve(API_NAME)
+
+if __name__ == '__main__':
+    # Heroku support: bind to PORT if defined, otherwise default to 5000.
+    if 'PORT' in environ:
+        port = int(environ.get('PORT'))
+        host = '0.0.0.0'
+    else:
+        port = 5000
+        host = '127.0.0.1'
+    api.run(host=host, port=port)
---
+++ settings.py
@@ -0,0 +1,32 @@
+"""Global API configuration."""
+
+from os import environ
+from urlparse import urlparse
+
+API_NAME = 'Taarifa'
+URL_PREFIX = 'api'
+if 'EVE_DEBUG' in environ:
+    DEBUG = True
+
+if 'MONGOLAB_URI' in environ:
+    url = urlparse(environ['MONGOLAB_URI'])
+    MONGO_HOST = url.hostname
+    MONGO_PORT = url.port
+    MONGO_USERNAME = url.username
+    MONGO_PASSWORD = url.password
+    MONGO_DBNAME = url.path[1:]
+else:
+    MONGO_DBNAME = API_NAME
+
+services = {
+    "item_title": "services",
+    "schema": {},
+    "resource_methods": ['GET', 'POST', 'DELETE'],
+}
+
+DOMAIN = {
+    'services': services,
+}
+
+# FIXME: Temporarily allow CORS requests for development purposes
+X_DOMAINS = "*"
