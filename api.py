08d8f2c30c810cda75961e5bf6025f1bf348fc02
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-21 20:34:05 +0100

    Add utility method to add documents

--- api.py
+++ api.py
@@ -1,10 +1,18 @@
+import json
 from os import environ
 
 from eve import Eve
 
-from settings import API_NAME
+from settings import API_NAME, URL_PREFIX
 
 api = Eve(API_NAME)
+
+
+def add_document(resource, document):
+    "Add a new document to the given resource."
+    return api.test_client().post('/' + URL_PREFIX + '/' + resource,
+                                  data=json.dumps(document),
+                                  content_type='application/json')
 
 if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
