082b8fe5ee1ac1a593f314663f2b34949f927257
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-21 23:25:30 +0100

    Define the schema for fields

--- schemas.py
+++ schemas.py
@@ -1,3 +1,84 @@
+field_schema = {
+    'type': {
+        'type': 'string',
+        'allowed': ['string',
+                    'boolean',
+                    'integer',
+                    'float',
+                    'number',
+                    'datetime',
+                    'dict',
+                    'list',
+                    'objectid',
+                    'file'],
+    },
+    'required': {
+        'type': 'boolean',
+        'default': False,
+    },
+    'readonly': {
+        'type': 'boolean',
+        'default': False,
+    },
+    'minlength': {
+        'type': 'integer',
+    },
+    'maxlength': {
+        'type': 'integer',
+    },
+    'min': {
+        'type': 'integer',
+    },
+    'max': {
+        'type': 'integer',
+    },
+    'allowed': {
+        'type': 'list',
+    },
+    'empty': {
+        'type': 'boolean',
+        'default': True,
+    },
+    'items': {
+        'type': 'list',
+    },
+    'scheam': {
+        'type': 'dict',
+    },
+    'unique': {
+        'type': 'boolean',
+        'default': False,
+    },
+    'data_relation': {
+        'type': 'dict',
+        'schema': {
+            'resource': {
+                'type': 'string',
+            },
+            'field': {
+                'type': 'string',
+            },
+            'embeddable': {
+                'type': 'boolean',
+                'default': False,
+            },
+            'version': {
+                'type': 'boolean',
+                'default': False,
+            },
+        },
+    },
+    'nullable': {
+        'type': 'boolean',
+        'default': False,
+    },
+    'default': {},
+    'versioned': {
+        'type': 'boolean',
+        'default': True,
+    },
+}
+
 service_schema = {
     'name': {
         'type': 'string',
@@ -6,6 +87,7 @@ service_schema = {
     'fields': {
         'type': 'dict',
         'required': True,
+        'keyschema': field_schema,
     },
     'unique': {
         'type': 'boolean',
