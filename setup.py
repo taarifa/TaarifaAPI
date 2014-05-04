267e7e3e40fe2143ba295bceeb117f91788f8bb7
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-04 15:35:48 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+dependency_links = ['git+https://github.com/nicolaiarocci/eve#egg=Eve-0.4-dev']
+setup(name='TaarifaAPI',
+      version='dev',
+      description='The Taarifa API for managing resources and requests',
+      long_description=open('README.rst').read(),
+      author='The Taarifa Organisation',
+      author_email='taarifadev@gmail.com',
+      url='http://taarifa.org',
+      download_url='https://github.com/taarifa/TaarifaAPI',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ],
+      packages=['taarifa_api'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['Eve==0.4-dev'],
+      dependency_links=dependency_links)
